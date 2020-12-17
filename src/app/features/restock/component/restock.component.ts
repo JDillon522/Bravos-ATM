32ea9290ebd015a5861f9277ec720c800494b059
Joseph Dillon <joseph.dillon.522@gmail.com>
2020-12-16 22:31:11 -0500

    restock ui

--- src/app/features/restock/component/restock.component.ts
+++ src/app/features/restock/component/restock.component.ts
@@ -34,6 +34,7 @@ export class RestockComponent implements OnInit, OnDestroy {
 
     return total;
   }
+  public manualDisable: boolean = false;
 
   constructor(
     private fb: FormBuilder,
@@ -61,10 +62,19 @@ export class RestockComponent implements OnInit, OnDestroy {
           this.transactionService.addRecord(res as Transaction);
 
           const val = this.currencyPipe.transform(this.totalNewCash);
+          this.manualDisable = true;
           const notification = this.snackBar.open(`Restocked ATM with ${val}`, 'X', { duration: 3000 });
           this.subscriptions.add(
             notification.afterDismissed().subscribe(() => {
-              this.form.reset(null);
+              this.form.reset({
+                100: 0,
+                50: 0,
+                20: 0,
+                10: 0,
+                5: 0,
+                1: 0
+              });
+              this.manualDisable = false;
             })
           );
         } else {
